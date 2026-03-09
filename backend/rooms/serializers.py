73c87ff13d55dafbd28b757115749d70e2ae8f11
Haozhe Zhang <1427214680@qq.com>
2026-03-09 20:23:01 +0000

    Update the backend model (rooms)
    1.add url & serializers
    2.create room and building model
    3.create room and buidling serializers for the rest django framework.
    4. create the simple function for the view (buildong and room lists)

---
+++ backend/rooms/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Building, Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    building = BuildingSerializer(read_only=True)
+    equipment = EquipmentSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Room
+        fields = ['id', 'name', 'capacity', 'location', 'is_active', 'building', 'equipment']
+
+
+class BuildingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Building
+        fields = ['id', 'name', 'campus_area', 'opening_hours']
